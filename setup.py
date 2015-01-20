0363b4ddeffe5d2db6273f5fbc54309eb0e4e6dc
Thomas Lim <txl11@ic.ac.uk>
2015-01-20 09:23:44 +0000

    Typo in setup.py. Change to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+# from distutils.core import setup
 
 setup(name='pyic_ldap',
       version='0.1',
@@ -7,6 +8,6 @@ setup(name='pyic_ldap',
       author='Thomas Lim',
       author_email='txl11@ic.ac.uk',
       license='MIT',
-      install_requires=['python-ldap']
+      install_requires=['python-ldap'],
       packages=['pyic_ldap'],
       )
